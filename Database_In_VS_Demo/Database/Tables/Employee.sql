41a9238c9f585cf6662b7e11a4a9f3ed778f70c3
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-08-30 09:38:10 +1000

    Add Database Project, Tables created

---
+++ Database_In_VS_Demo/Database/Tables/Employee.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[StaffID] INT, 
+    [GivenName] NCHAR(100) NULL, 
+    [SurName] NCHAR(100) NULL, 
+    CONSTRAINT [PK_Employee] PRIMARY KEY ([StaffID]) 
+)
---
+++ Database_In_VS_Demo/Database/Tables/Office.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Office]
+(
+	[LocCode] INT NOT NULL, 
+    [Address] NCHAR(200) NULL, 
+    [Description] NCHAR(100) NULL, 
+    CONSTRAINT [PK_Office] PRIMARY KEY ([LocCode])
+)
