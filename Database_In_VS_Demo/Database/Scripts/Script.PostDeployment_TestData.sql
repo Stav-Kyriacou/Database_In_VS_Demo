956f55099dd2c8db0a6f366e010bedece1c4824b
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-08-30 10:15:48 +1000

    deploy test data

--- Database_In_VS_Demo/Database/Scripts/Script.PostDeployment_TestData.sql
+++ Database_In_VS_Demo/Database/Scripts/Script.PostDeployment_TestData.sql
@@ -9,21 +9,23 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
-
+DELETE FROM Employee;
+DELETE FROM Office;
+DELETE FROM Assignment;
 -- employee
 INSERT INTO Employee (StaffID, GivenName, SurName) VALUES
 (1, 'Meme', 'Machine'),
 (2, 'Bibius', 'Maximus'),
-(3, 'Person', 'Person')
+(3, 'Person', 'Person');
 
 -- office
 INSERT INTO Office (LocCode, Address, Description) VALUES
 (101, '123 Street St', 'Something'),
 (102, '345 Street St', 'Somewhere'),
-(103, '567 Street St', 'Elsewhere')
+(103, '567 Street St', 'Elsewhere');
 
 -- assignment
 INSERT INTO Assignment (StaffID, LocCode, DateAssigned) VALUES
 (1, 101, CONVERT(DATE, '3/21/2021', 101)),
 (2, 102, CONVERT(DATE, '8/30/2021', 101)),
-(3, 103, CONVERT(DATE, '6/1/2021', 101))
+(3, 103, CONVERT(DATE, '6/1/2021', 101));
